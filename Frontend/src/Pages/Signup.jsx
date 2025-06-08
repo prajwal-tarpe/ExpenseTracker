c69e375a681fcbc317f9d473215d87904db91c10
prajwal-tarpe <141891033+prajwal-tarpe@users.noreply.github.com>
2025-06-08 09:16:50 +0530

    Update Signup.jsx

--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -11,7 +11,6 @@ function Signup() {
   });
   const handleChange = (e) => {
     const {name,value} = e.target;
-    console.log(name,value);
     const copySignupInfo = {...signupInfo};
     copySignupInfo[name] = value;
     setSignupInfo(copySignupInfo);
