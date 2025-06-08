1d70716bcf6b4137c19b32f873f0e1380a17dbca
prajwal-tarpe <141891033+prajwal-tarpe@users.noreply.github.com>
2025-06-08 09:16:28 +0530

    Update Login.jsx

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -11,7 +11,6 @@ function Login() {
 
   const handleChange = (e) => {
     const {name,value} = e.target;
-    console.log(name,value);
     const copyLoginInfo = {...loginInfo};
     copyLoginInfo[name] = value;
     setLoginInfo(copyLoginInfo);
